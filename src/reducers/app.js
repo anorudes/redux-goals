6a6eca1302d86a8401613df4f12ee5b4fcd116ee
anorudes <anorudes@gmail.com>
2015-10-14 19:48:15 +0500

    cleanup

--- src/reducers/app.js
+++ src/reducers/app.js
@@ -1,14 +1,8 @@
 const initialState = {
-  activePage: 'home',
 };
 
 export function app(state = initialState, action) {
   switch (action.type) {
-  case 'CHANGE':
-    return {
-      ...state,
-      activePage: action.page,
-    };
   default:
     return state;
   }
